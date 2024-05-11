3fc3510eafb955c0e106452d7c3d2d873781cc3b
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-05-11 12:49:51 +0200

    Fix error

--- src/FormExample.jsx
+++ src/FormExample.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Form, Field } from 'react-final-form'
 import { persistStore, persistReducer } from 'redux-persist'
 
-persistStore();
-persistReducer();
-
 export const FormExample = () => {
   const onSubmit = (e) => { /* fonction appelée lorsque le formulaire est soumis */ }
   //const validate = (e) => { /* fonction qui retourne les erreurs de validation */ }
@@ -23,10 +20,10 @@ const validate = e => {
         <form onSubmit={handleSubmit}>
           <h2>Titre du formulaire</h2>
           <div>
-            <label>Prénom</label>
-            <Field name="firstName" component="input" placeholder="First Name" />
+            <label className="margin-small">Prénom</label>
+            <Field className="margin-small" name="firstName" component="input" placeholder="First Name" />
           </div>
-          <button type="submit">Submit</button>
+          <button className="margin-small" type="submit">Submit</button>
         </form>
       )}
     />
